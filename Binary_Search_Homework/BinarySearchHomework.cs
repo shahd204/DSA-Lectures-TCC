3ffa1cee81f68cc6ab904d55d3bf2bb7004b05f2
shahd alst <Shahd998@gmail.com>
2023-10-27 16:47:29 +0200

    shahd alst and noor darkabani

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -13,25 +13,75 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
         {
+            if (end >= start)
+            {
+                int mid1 = start + (end - start) / 3;
+                int mid2 = end - (end - start) / 3;
+
+                if (arr[mid1] == key)
+                    return mid1;
+
+                if (arr[mid2] == key)
+                    return mid2;
+
+                if (key < arr[mid1])
+                    return TernarySearch(arr, key, start, mid1 - 1);
+
+                if (key > arr[mid2])
+                    return TernarySearch(arr, key, mid2 + 1, end);
+
+                else
+                    return TernarySearch(arr, key, mid1 + 1, mid2 - 1);
+            }
+            return-1;
+
+            }
             //TODO: write code for the ternary search algorithm and return the index of the element
-            return -1;
-        }
+          
+        
 
-        public static int BinarySearchForCalculatingRepeated
-            (int[] arr, int key, bool is_first, int start, int end) // don't edit this line!!!
-                                                                    // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
-                                                                    // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
-                                                                    // من الممكن تمرير قيمة 0 لتجاهلهم
+        public static int BinarySearchForCalculatingRepeated(int[] arr, int key, bool is_first, int start, int end) // don't edit this line!!!
+                                                                                                                    // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
+                                                                                                                    // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
+                                                                                                                    // من الممكن تمرير قيمة 0 لتجاهلهم
         {
+           if (start > end)
+                return -1;
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] < key)
+                return BinarySearchForCalculatingRepeated(arr, key, is_first, mid + 1, end);
+
+            if (arr[mid] > key)
+                return BinarySearchForCalculatingRepeated(arr, key, is_first, start, mid - 1);
+
+            if (is_first && (mid == 0 || arr[mid - 1] != key))
+                return mid;
+
+            if (!is_first && (mid == arr.Length - 1 || arr[mid + 1] != key))
+                return mid;
+
+            if (is_first)
+                return BinarySearchForCalculatingRepeated(arr, key, is_first, start, mid - 1);
+
+            return BinarySearchForCalculatingRepeated(arr, key, is_first, mid + 1, end);
+
+            }
             //TODO: this methods is for getting the first accurence of the key and the last accurance
-            return -1;
-        }
+            public static int GetrepeatCount(int[] arr,int key)
+                {
 
-        public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
-        {
-            //TODO: write code to calculate the repeat count of a spacific element
-            // make sure to use the previous method in this method
-            return -1;
-        }
+                   int FirstAcord = BinarySearchForCalculatingRepeated(arr, key, true, 0, arr.Length - 1);
+          int LastAcord = BinarySearchForCalculatingRepeated(arr, key, false, 0, arr.Length - 1);
+           if (FirstAcord != -1)
+            return LastAcord - FirstAcord + 1;
+              else
+               return 0;
+            
+            }        
+            //TODO: write code to calculate the repeat count of a spacific element 
+            //make sure to use the previous method in this method
+       }
     }
 }
